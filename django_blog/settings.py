f571117c762b16bc8fa575095b5ee1aeaa93d906
daxit99 <73168093+daxit99@users.noreply.github.com>
2021-09-05 18:24:36 +0500

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -144,8 +144,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='daku95109@gmail.com'
-EMAIL_HOST_PASSWORD = 'Daxit@123'
+EMAIL_HOST_USER =''
+EMAIL_HOST_PASSWORD = ''
 
 
 django_heroku.settings(locals())
